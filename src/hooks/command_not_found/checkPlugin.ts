349a6c0bea2b973fe13e9b1b6ff61a2cd2fe54b3
Ryan Garant <ryantgarant@gmail.com>
2018-08-22 23:26:40 -0700

    Add check if plugin exists function

--- src/hooks/command_not_found/checkPlugin.ts
+++ src/hooks/command_not_found/checkPlugin.ts
@@ -1,8 +1,18 @@
 import { Hook } from '@oclif/config'
+const fs = require('fs')
+const npmGlobalModulePath = require('global-modules')
 
 const hook: Hook<'command_not_found'> = async function(opts) {
-  process.stdout.write(opts)
+  checkIfPluginExists(opts.id)
   this.exit()
+}
+
+function checkIfPluginExists(cmd) {
+  const globalNpmPackages: string[] = fs.readdirSync(npmGlobalModulePath)
+
+  const pluginPackageName = globalNpmPackages.find(npmPackage => npmPackage.includes(`gh-${cmd}`))
+
+  console.log('npmGlobalModulePath', pluginPackageName)
 }
 
 export default hook
