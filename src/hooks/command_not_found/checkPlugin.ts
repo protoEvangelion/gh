f59f99a5783da4cfd66ba098383f2aaae27222e4
Ryan Garant <ryantgarant@gmail.com>
2018-08-23 20:53:13 -0700

    Getting plugins to work

--- src/hooks/command_not_found/checkPlugin.ts
+++ src/hooks/command_not_found/checkPlugin.ts
@@ -1,18 +1,28 @@
 import { Hook } from '@oclif/config'
+// import { log } from '../../logger'
+
 const fs = require('fs')
+const path = require('path')
 const npmGlobalModulePath = require('global-modules')
 
 const hook: Hook<'command_not_found'> = async function(opts) {
-  checkIfPluginExists(opts.id)
+  const pluginName = getPluginName(opts.id)
+  // if (pluginName) {
+  // const pluginModule = require(path.join(npmGlobalModulePath, pluginName, 'bin', 'jira.js'))
+  console.log('pluginName', path.join(npmGlobalModulePath, pluginName, 'bin', 'jira.js'))
+
+  //   console.log('package', pluginModule)
+  // } else {
+  //   log('Cmd or plugin does not exist')
+  // }
+
   this.exit()
 }
 
-function checkIfPluginExists(cmd) {
+function getPluginName(cmd): string | undefined {
   const globalNpmPackages: string[] = fs.readdirSync(npmGlobalModulePath)
 
-  const pluginPackageName = globalNpmPackages.find(npmPackage => npmPackage.includes(`gh-${cmd}`))
-
-  console.log('npmGlobalModulePath', pluginPackageName)
+  return globalNpmPackages.find(npmPackage => npmPackage.includes(`gh-${cmd}`))
 }
 
 export default hook
