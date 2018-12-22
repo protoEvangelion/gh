40e6e6874c95eb7628f354349f16eb35fc2f101b
Ryan Garant <ryantgarant@gmail.com>
2018-12-22 11:15:36 -0800

    test17

--- __tests__/issue.test.js
+++ __tests__/issue.test.js
@@ -6,13 +6,29 @@
 
 const { runCmd } = require('./testUtils')
 
-describe('E2E: Issues Module Test', () => {
+let describeIf
+
+if (process.env.CONTINUOUS_INTEGRATION && process.env.TRAVIS_EVENT_TYPE !== 'push') {
+    describeIf = describe.skip
+} else {
+    describeIf = describe
+}
+
+describeIf('E2E: Issues Module Test', () => {
     it('List Issues `gh is`', done => {
-        expect(runCmd('bin/gh.js is')).toMatchSnapshot()
+        expect(runCmd('bin/gh.js is')).toMatchInlineSnapshot(`
+"Listing open issues on protoevangelion/gh
+#43 testGo @protoEvangelion (a year ago)
+
+#42 test2 @protoEvangelion (a year ago)
+
+#38 Node GH rocks! @protoEvangelion (a year ago)
+"
+`)
         done()
     })
 
-    it('Comment on Issuess `gh is 1 -c "test"`', done => {
+    it('Comment on Issues `gh is 1 -c "test"`', done => {
         expect(runCmd(`bin/gh.js is 1 -c "test"`)).toMatchSnapshot()
         done()
     })
